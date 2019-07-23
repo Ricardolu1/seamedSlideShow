7012aee11e62ae43a70b4e7b0b9c80ba2cfc607f
ycy <568082704@qq.com>
2019-07-23 22:38:49 +0800

    支持无限张图片的轮播

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
-$(p1).on('click',function() {
-  $(images).css({
-    transform:'translateX(0)'
-  })
-})
+// $(p1).on('click',function() {
+//   $(images).css({
+//     transform:'translateX(0)'
+//   })
+// })
 
-$(p2).on('click',function() {
-  $(images).css({
-    transform:'translateX(-300px)'
-  })
-})
+// $(p2).on('click',function() {
+//   $(images).css({
+//     transform:'translateX(-300px)'//有点像给对象的属性赋值
+//   })
+// })
 
-$(p3).on('click',function() {
-  $(images).css({
-    transform:'translateX(-600px)'
-  })
-})
+// $(p3).on('click',function() {
+//   $(images).css({
+//     transform:'translateX(-600px)'
+//   })
+// })
 
+var allButtons=$('#buttons>button')
+for (let i = 0; i < allButtons.length; i++) {
+  $(allButtons[i]).on('click',function(x) {
+    let index = $(x.currentTarget).index()
+    let n = (-300)*index
+    $('#images').css({
+      transform:'translateX(' + n + 'px)'
+    })
+  });
+}
 
 
 
